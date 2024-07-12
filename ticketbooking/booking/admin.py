f32dff01d9c90741ca1cd420f2906bc8ab84a9c4
unknown <charang2003@gmail.com>
2024-07-12 15:57:39 +0530

    updated amdin.py and models.py

--- ticketbooking/booking/admin.py
+++ ticketbooking/booking/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Event, Ticket, Booking
 
-# Register your models here.
+admin.site.register(Event)
+admin.site.register(Ticket)
+admin.site.register(Booking)
--- ticketbooking/booking/models.py
+++ ticketbooking/booking/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Event(models.Model):
+    name = models.CharField(max_length=100)
+    date = models.DateTimeField()
+    location = models.CharField(max_length=100)
+    total_tickets = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
+
+class Ticket(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f'{self.event.name} - ${self.price}'
+
+class Booking(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    ticket = models.ForeignKey(Ticket, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    booking_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user.username} - {self.ticket.event.name}'
