8b91119cc6f75d3796f4e15111672174cd29fdb3
unknown <charang2003@gmail.com>
2024-07-12 16:29:24 +0530

    redefined file structure

--- ticketbooking/booking/urls.py
+++ ticketbooking/booking/urls.py
@@ -1,10 +1,11 @@
+# booking/urls.py
+
 from django.urls import path
 from . import views
 
-app_name = 'booking'
-
 urlpatterns = [
-    path('', views.event_list, name='event_list'),
+    path('', views.home, name='home'),
+    path('events/', views.event_list, name='event_list'),
     path('event/<int:event_id>/', views.event_detail, name='event_detail'),
     path('ticket/<int:ticket_id>/book/', views.book_ticket, name='book_ticket'),
     path('bookings/', views.booking_list, name='booking_list'),
